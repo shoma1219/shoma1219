97ac063f74ef6d7f89903f6b7a11d2bb7c475ccd
shoma1219 <shoma.s.1219@gmail.com>
2021-07-30 23:17:18 +0900

    update

--- scroll_interaction/js/main.js
+++ scroll_interaction/js/main.js
@@ -4,23 +4,17 @@ const container = document.querySelector('.container');
 const center_box = document.querySelector('.center_box');
 
 const box1 = document.querySelector('.box1');
-const text1 = document.querySelector('.text1');
-
 const box2 = document.querySelector('.box2');
-const text2 = document.querySelector('.text2');
 
 let height = window.innerHeight;
-// console.log(height);
 let ticking = false;
 
 function doSomething(scroll_pos) {
     center_box.style.transform = `rotate(${map(scroll_pos, 0, height, 0, 360)}deg)`;
+    center_box.style.background = `rgb(${255 - map(scroll_pos, 0, height, 0, 37)}, ${255 - map(scroll_pos, 0, height, 0, 114)}, ${255 - map(scroll_pos, 0, height, 0, 114)})`;
 
     box1.style.left = - map(scroll_pos, 0, height, 0, 150) + 'px';
-    text1.style.opacity = 1 - map(scroll_pos, 0, height, 0, 1);
-
     box2.style.left = -150 + map(scroll_pos, 0, height, 0, 150) + 'px';
-    text2.style.opacity = 0 + map(scroll_pos, 0, height, 0, 1);
 }
 
 container.addEventListener('scroll', function (e) {
