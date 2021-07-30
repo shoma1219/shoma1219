271c099de65a68eb8a0cc0118e84d522744cb762
shoma1219 <shoma.s.1219@gmail.com>
2021-07-31 02:37:50 +0900

    修正

--- scroll_interaction/js/main.js
+++ scroll_interaction/js/main.js
@@ -1,24 +1,55 @@
 'use strict';
 
+const background = document.querySelector('.background');
+
 const container = document.querySelector('.container');
 const center_box = document.querySelector('.center_box');
 
 const box1 = document.querySelector('.box1');
 const box2 = document.querySelector('.box2');
+const box3 = document.querySelector('.box3');
+const box4 = document.querySelector('.box4');
 
 let height = window.innerHeight;
-let ticking = false;
 
-function doSomething(scroll_pos) {
-    center_box.style.transform = `rotate(${map(scroll_pos, 0, height, 0, 360)}deg)`;
-    center_box.style.background = `rgb(${255 - map(scroll_pos, 0, height, 0, 37)}, ${255 - map(scroll_pos, 0, height, 0, 114)}, ${255 - map(scroll_pos, 0, height, 0, 114)})`;
+function doSomething(scroll_top) {
+    // background.style.background = `rgb(${100 - map(scroll_top, 0, height, 0, 37)}, ${100 - map(scroll_top, 0, height, 0, 37)}, ${100 - map(scroll_top, 0, height, 0, 37)})`;
 
-    box1.style.left = - map(scroll_pos, 0, height, 0, 150) + 'px';
-    box2.style.left = -150 + map(scroll_pos, 0, height, 0, 150) + 'px';
+
+    if (scroll_top <= height) {
+        center_box.style.transform = `translate(-50%, -50%) rotate(${map(scroll_top, 0, height, 0, 360)}deg)`;
+        center_box.style.background = `rgb(${255 - map(scroll_top, 0, height, 0, 37)}, ${255 - map(scroll_top, 0, height, 0, 114)}, ${255 - map(scroll_top, 0, height, 0, 114)})`;
+        box1.style.left = - map(scroll_top, 0, height, 0, 100) + 'px';
+        box2.style.left = -100 + map(scroll_top, 0, height, 0, 100) + 'px';
+        console.log('1');
+        console.log(box1.style.left);
+        console.log(box2.style.left);
+        console.log(box3.style.left);
+        console.log(box4.style.left);
+    } else if (scroll_top - height <= height) {
+        center_box.style.transform = `translate(-50%, -50%) rotate(${map(scroll_top - height, 0, height, 0, 360)}deg)`;
+        box2.style.left = - map(scroll_top - height, 0, height, 0, 100) + 'px';
+        box3.style.left = -100 + map(scroll_top - height, 0, height, 0, 100) + 'px';
+        console.log('2');
+        console.log(box1.style.left);
+        console.log(box2.style.left);
+        console.log(box3.style.left);
+        console.log(box4.style.left);
+    } else if (scroll_top - height * 2 <= height) {
+        center_box.style.transform = `translate(-50%, -50%) rotate(${map(scroll_top - height * 2, 0, height, 0, 360)}deg)`;
+        box3.style.left = - map(scroll_top - height * 2, 0, height, 0, 100) + 'px';
+        box4.style.left = -100 + map(scroll_top - height * 2, 0, height, 0, 100) + 'px';
+        console.log('3');
+        console.log(box1.style.left);
+        console.log(box2.style.left);
+        console.log(box3.style.left);
+        console.log(box4.style.left);
+    }
 }
 
+let ticking = false;
 container.addEventListener('scroll', function (e) {
-    console.log(container.scrollTop);
+    // console.log(container.scrollTop);
 
     if (!ticking) {
         window.requestAnimationFrame(function () {
