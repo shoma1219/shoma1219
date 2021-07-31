b34ecdd981605795b2ce7ceb86822b0b4b46dd39
shoma1219 <shoma.s.1219@gmail.com>
2021-07-31 19:51:14 +0900

    update

--- scroll_interaction/js/main.js
+++ scroll_interaction/js/main.js
@@ -1,59 +1,47 @@
 'use strict';
 
-const background = document.querySelector('.background');
+const scrollAnimationContainer = document.querySelector('.scrollAnimationContainer');
+const centerBox = document.querySelector('.scrollAnimationContainer__centerBox');
+const box1 = document.querySelector('.scrollAnimationContainer__box1');
+const box2 = document.querySelector('.scrollAnimationContainer__box2');
+const box3 = document.querySelector('.scrollAnimationContainer__box3');
+const box4 = document.querySelector('.scrollAnimationContainer__box4');
 
-const container = document.querySelector('.container');
-const center_box = document.querySelector('.center_box');
+const contentContainer = document.querySelector('.contentContainer');
+let height = contentContainer.clientHeight;
 
-const box1 = document.querySelector('.box1');
-const box2 = document.querySelector('.box2');
-const box3 = document.querySelector('.box3');
-const box4 = document.querySelector('.box4');
-
-let height = window.innerHeight;
-
-function doSomething(scroll_top) {
-    // background.style.background = `rgb(${100 - map(scroll_top, 0, height, 0, 37)}, ${100 - map(scroll_top, 0, height, 0, 37)}, ${100 - map(scroll_top, 0, height, 0, 37)})`;
-
+function scrollAnimation(scroll_top) {
 
     if (scroll_top <= height) {
-        center_box.style.transform = `translate(-50%, -50%) rotate(${map(scroll_top, 0, height, 0, 360)}deg)`;
-        center_box.style.background = `rgb(${255 - map(scroll_top, 0, height, 0, 37)}, ${255 - map(scroll_top, 0, height, 0, 114)}, ${255 - map(scroll_top, 0, height, 0, 114)})`;
+
+        scrollAnimationContainer.style.background = `rgb(${100 - map(scroll_top, 0, height, 0, 37)}, ${100 - map(scroll_top, 0, height, 0, 37)}, ${100 - map(scroll_top, 0, height, 0, 37)})`;
+        centerBox.style.transform = `translate(-50%, -50%) rotate(${map(scroll_top, 0, height, 0, 360)}deg)`;
+        centerBox.style.background = `rgb(${255 - map(scroll_top, 0, height, 0, 37)}, ${255 - map(scroll_top, 0, height, 0, 114)}, ${255 - map(scroll_top, 0, height, 0, 114)})`;
         box1.style.left = - map(scroll_top, 0, height, 0, 100) + 'px';
         box2.style.left = -100 + map(scroll_top, 0, height, 0, 100) + 'px';
-        console.log('1');
-        console.log(box1.style.left);
-        console.log(box2.style.left);
-        console.log(box3.style.left);
-        console.log(box4.style.left);
+
     } else if (scroll_top - height <= height) {
-        center_box.style.transform = `translate(-50%, -50%) rotate(${map(scroll_top - height, 0, height, 0, 360)}deg)`;
+
+        centerBox.style.transform = `translate(-50%, -50%) rotate(${map(scroll_top - height, 0, height, 0, 360)}deg)`;
         box2.style.left = - map(scroll_top - height, 0, height, 0, 100) + 'px';
         box3.style.left = -100 + map(scroll_top - height, 0, height, 0, 100) + 'px';
-        console.log('2');
-        console.log(box1.style.left);
-        console.log(box2.style.left);
-        console.log(box3.style.left);
-        console.log(box4.style.left);
+
     } else if (scroll_top - height * 2 <= height) {
-        center_box.style.transform = `translate(-50%, -50%) rotate(${map(scroll_top - height * 2, 0, height, 0, 360)}deg)`;
+
+        centerBox.style.transform = `translate(-50%, -50%) rotate(${map(scroll_top - height * 2, 0, height, 0, 360)}deg)`;
         box3.style.left = - map(scroll_top - height * 2, 0, height, 0, 100) + 'px';
         box4.style.left = -100 + map(scroll_top - height * 2, 0, height, 0, 100) + 'px';
-        console.log('3');
-        console.log(box1.style.left);
-        console.log(box2.style.left);
-        console.log(box3.style.left);
-        console.log(box4.style.left);
+
     }
 }
 
 let ticking = false;
-container.addEventListener('scroll', function (e) {
-    // console.log(container.scrollTop);
+contentContainer.addEventListener('scroll', function (e) {
+    // console.log(contentContainer.scrollTop);
 
     if (!ticking) {
         window.requestAnimationFrame(function () {
-            doSomething(container.scrollTop);
+            scrollAnimation(contentContainer.scrollTop);
             ticking = false;
         });
 
@@ -61,19 +49,14 @@ container.addEventListener('scroll', function (e) {
     }
 });
 
-// map関数
+// map
 const map = (value, fromMin, fromMax, toMin, toMax) => {
-
     let result = 0;
-
     result = (value <= fromMin)
         ? toMin : (value >= fromMax)
             ? toMax : (() => {
-
                 let ratio = (toMax - toMin) / (fromMax - fromMin);
                 return (value - fromMin) * ratio + toMin;
-
             })();
-
     return result;
 };
